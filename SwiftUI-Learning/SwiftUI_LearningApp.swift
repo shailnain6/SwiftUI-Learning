7092587825eca7f41b7ac6573dab8a828024b7b1
Shailendra Nain <shailnain.6@gmail.com>
2025-05-05 21:10:17 -0400

    Initial Commit

---
+++ SwiftUI-Learning/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  SwiftUI-Learning
+//
+//  Created by Shailendra Nain on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
