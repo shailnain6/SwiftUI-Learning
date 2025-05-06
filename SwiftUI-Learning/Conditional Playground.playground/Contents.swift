10dd7d02a9ce6d0fdd973fd936d64b1710cb59f1
Shailendra Nain <shailnain.6@gmail.com>
2025-05-05 22:27:38 -0400

    Playground added with if else conditional

---
+++ SwiftUI-Learning/Conditional Playground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var messageString = "I am a Developer!"
+//print("Hello There!", messageString) adds a space
+//print("Hello There!" + messageString) concatenates Strings
+//print("Hello There!" \(messageString)) String interpolation
+
+var person = "Shail"
+
+//Conditionals Starts Here..
+
+if person == "Limor" {
+    print("Hello, Lady Ada!")
+} else if person == "Grace" {
+    print("Hello, Admiral Hopper")
+} else {
+    print("Hey There! \(person)")
+}
