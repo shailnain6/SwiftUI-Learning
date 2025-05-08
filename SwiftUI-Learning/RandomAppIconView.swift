9024e62eed1ce32d53f329e4d7d0d5977a00d85f
Shailendra Nain <shailnain.6@gmail.com>
2025-05-07 21:28:27 -0400

    Basic Image animation added

--- SwiftUI-Learning/RandomAppIconView.swift
+++ SwiftUI-Learning/RandomAppIconView.swift
@@ -11,6 +11,7 @@ struct RandomAppIconView: View {
     @State private var image = 0
     @State private var apps = ["App Store", "TV", "Books", "Calculator", "Calendar", "Camera", "Clips", "Clock", "Contacts", "Facetime", "Apple Store"]
     @State private var isRandomBtnClicked = false
+    @State private var appName = ""
     
     func generateRandomNumber(){
         let randomNumber = Int.random(in: 1...11)
@@ -28,10 +29,12 @@ struct RandomAppIconView: View {
                 .scaledToFit()
                 .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 .shadow(radius: 30)
+                .animation(.default, value: image)
             Text(image == 0 ? "" : apps[image - 1])
                 .foregroundStyle(.indigo)
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+//                .animation(.default, value: apps[image - 1])
             
             Spacer()
             HStack {
